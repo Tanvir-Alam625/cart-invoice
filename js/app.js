60f501521d885956b547fb867c0d8bec9cc8aa10
tanveer <mdtanviralamht731@gmail.com>
2022-02-12 15:53:38 +0600

    app.js file added

--- js/app.js
+++ js/app.js
@@ -11,3 +11,29 @@ const detailsInputValue = detailsInput.value;
 document.getElementById('buyer-info').innerText = detailsInputValue;
 console.log(detailsInput);
 });
+function getInputValue(inputId){
+    const input = document.getElementById(inputId);
+    const inputValue = input.value;
+    input.value = '';
+    return inputValue;
+}
+// add btn event handler 
+addButton.addEventListener('click',function(){
+    const tableValue = document.getElementById('info-table');
+    // getInputValue('item-name-input');
+    const itemPrice = getInputValue('item-price-input');
+    const itemQuantity = getInputValue('item-quantity-input');
+    const tr = document.createElement('tr');
+    const totalPrice = itemPrice * itemQuantity;
+
+    const tableData = `
+            <th >${getInputValue('item-name-input')}</th>
+            <td>${itemPrice}</td>
+            <td>${itemQuantity}</td>
+            <td class ="total-price">${totalPrice}</td>
+`;
+console.log(tableData);
+tr.innerHTML = tableData;
+tableValue.appendChild(tr);
+
+})
